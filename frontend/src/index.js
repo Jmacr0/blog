d5ab50b03bc11dde58427de5bd2b071324c5d1db
Jmacro <jonmacrohon@gmail.com>
2021-03-18 23:31:40 +1100

    fe

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "./utils/apolloClient";
+import './index.css';
+import App from './components/app/App';
+
+ReactDOM.render(
+	<Router>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</Router>,
+	document.getElementById('root')
+);
